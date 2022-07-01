e99ae26daf4ba9f6e2c5bd2ffb6fd6ccdfb23901
VladyslavPonomaryov <vladyslav.ponomaryov@ab-inbev.com>
2022-07-01 16:41:39 +0300

    Add requirements and tests with 1 fail

---
+++ app/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+# Create your tests here.
+class TestTest(TestCase):
+
+    def test_pass(self):
+        self.assertTrue(True)
+
+    def test_fail(self):
+        self.assertTrue(False)
