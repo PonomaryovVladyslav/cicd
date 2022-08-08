260792e103407a7329b4ab2f5f949d50e7a72d69
VladyslavPonomaryov <vladyslav.ponomaryov@ab-inbev.com>
2022-08-09 01:13:24 +0200

    test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 
@@ -6,3 +7,12 @@ class TestTest(TestCase):
 
     def test_pass(self):
         self.assertTrue(True)
+
+    def test_user(self):
+        username = 'shetu'
+        password = 'hello'
+        u = User(username=username)
+        u.set_password(password)
+        u.save()
+        self.assertEqual(u.username, username)
+        self.assertTrue(u.check_password(password))
